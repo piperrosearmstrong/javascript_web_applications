d01752cc6865685045c0d1c0c491bd8adf5f3df1
Piper-Rose Armstrong <piperrosearmstrong@gmail.com>
2022-10-26 13:43:22 +0100

    Handling user interaction

---
+++ example-3/bundle.js
@@ -0,0 +1,38 @@
+(() => {
+  var __commonJS = (cb, mod) => function __require() {
+    return mod || (0, cb[Object.keys(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+  };
+
+  // messageView.js
+  var require_messageView = __commonJS({
+    "messageView.js"(exports, module) {
+      var MessageView2 = class {
+        constructor() {
+          this.buttonEl = document.querySelector("#show-message-button");
+          this.hideButtonEl = document.querySelector("#hide-message-button");
+          this.buttonEl.addEventListener("click", () => {
+            this.displayMessage();
+          });
+          this.hideButtonEl.addEventListener("click", () => {
+            this.hideMessage();
+          });
+        }
+        displayMessage() {
+          console.log("Thanks for clicking me!");
+          const messageElement = document.createElement("div");
+          messageElement.id = "message";
+          messageElement.innerText = "This message displayed by Javascript";
+          document.querySelector("#main-container").append(messageElement);
+        }
+        hideMessage() {
+          document.querySelector("#message").remove();
+        }
+      };
+      module.exports = MessageView2;
+    }
+  });
+
+  // index.js
+  var MessageView = require_messageView();
+  var view = new MessageView();
+})();
